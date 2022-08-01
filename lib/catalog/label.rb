451c2fb1cab358af6e17029754adfef475b1c93a
Kelvin Bush <kelybush@gmail.com>
2022-08-01 17:08:36 +0300

    clean up

--- lib/catalog/label.rb
+++ lib/catalog/label.rb
@@ -1,1 +1,18 @@
-require_relative 'item'class Label  attr_reader :title, :color, :items  def initialize(title, color)    @id = Random.rand(1..1000)    @items = []    @title = title    @color = color  end  def add_item(item: Item)    @items.push(item)    item.add_label(self)  endend
+require_relative 'item'
+
+class Label
+  attr_reader :title, :color, :items
+
+  def initialize(title, color)
+    @id = Random.rand(1..1000)
+    @items = []
+    @title = title
+    @color = color
+  end
+
+  def add_item(item: Item)
+    @items.push(item)
+    item.add_label(self)
+  end
+
+end
