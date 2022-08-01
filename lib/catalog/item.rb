f0b006da6ff418aa1d61e96ac70e2d26f524d8fa
Kelvin Bush <kelybush@gmail.com>
2022-08-01 16:29:10 +0300

    set up item class properties

--- lib/catalog/item.rb
+++ lib/catalog/item.rb
@@ -1,1 +1,1 @@
-class Item  attr_reader :publish_date, :label, :date, :genre, :author  def initialize(date)    @id = Random.rand(1..1000)    @publish_date = date    @archived = false  endend
+class Item  attr_reader :publish_date, :label, :date, :genre, :author  def initialize(date)    @id = Random.rand(1..1000)    @publish_date = date    @archived = false  end  def can_be_archived?  end  def move_to_archive  end  private :can_be_archived?end
