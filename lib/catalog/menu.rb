f8fb62b4c7bce30436b0e510feecef0d4b71ac83
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-08-01 22:15:31 +0100

    Modified menu.rb file

--- lib/catalog/menu.rb
+++ lib/catalog/menu.rb
@@ -24,4 +24,30 @@ class Menu
     end
   end
 
+  # rubocop:disable Metrics/CyclomaticComplexity
+
+  def execute_inputs
+    input = Integer(gets.chomp)
+    case input
+    when 1 then puts 'Catalog is empty! Choose (9) to add a book.'
+    when 2 then puts 'Album is empty! Choose (10) to add a music album.'
+    when 3 then puts 'Gallery is empty! Choose (11) to add a movie.'
+    when 4 then puts 'Store is empty! Choose (12) to add a game.'
+    when 5 then puts 'Genre\'s list is empty!'
+    when 6 then puts 'Labels\'s list is empty!'
+    when 7 then puts 'Author\'s list is empty!'
+    when 8 then puts 'Source\'s list is empty!'
+    when 9 then puts 'Please add a book!'
+    when 10 then puts 'Please add a music album!'
+    when 11 then puts 'Please add a movie!'
+    when 12 then puts 'Please add a game'
+    when 13
+      puts 'Thank you for using this app. Goodbye'
+      exit
+    else
+      puts 'Kindly enter a number between 1-13'
+    end
+  end
+
+  # rubocop:enable Metrics/CyclomaticComplexity
 end
