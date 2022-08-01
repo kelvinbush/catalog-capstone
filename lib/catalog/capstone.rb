95d26f7cf3494350cd098dfaf22cae8376d2444d
Kelvin Bush <kelybush@gmail.com>
2022-08-01 12:46:13 +0300

    Starting files

---
+++ lib/catalog/capstone.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "capstone/version"
+
+module Catalog
+  module Capstone
+    class Error < StandardError; end
+  end
+end
---
+++ lib/catalog/capstone/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Catalog
+  module Capstone
+    VERSION = "0.1.0"
+  end
+end
