1e85e7c5a12e64114ea1708d3f0ae94bd187f797
Kelvin Bush <kelybush@gmail.com>
2022-08-02 04:53:57 +0300

    test for can_archive method in book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,9 +7,21 @@ describe Book do
     @new_book = Book.new('2018-01-01', 'John Terry', 'bad')
   end
 
-  context 'when condition a new Book is created' do
+  context 'when a new Book is created' do
     it 'should be an instance of book class' do
       expect(@new_book).to be_instance_of Book
+    end
+
+    it 'should have the correct publisher' do
+      expect(@book.publisher).to eq 'Kelvin Bush'
+    end
+
+    it 'should archive when older than 10 years' do
+      expect(@old_book.can_be_archived?).to be_truthy
+    end
+
+    it 'should archive when cover_state is bad' do
+      expect(@new_book.can_be_archived?).to be_truthy
     end
   end
 end
