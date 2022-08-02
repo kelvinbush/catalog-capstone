612bd6a3f2ed19ba30325cbe3a65d03adf7072e5
SimbaWANGU <simbawangulu@gmail.com>
2022-08-02 16:50:30 +0300

    add one to many relationship between autho and item

--- lib/author.rb
+++ lib/author.rb
@@ -8,5 +8,6 @@ class Author
 
   def add_item(item)
     @items.push(item)
+    item.add_author(self)
   end
 end
--- lib/item.rb
+++ lib/item.rb
@@ -28,6 +28,11 @@ class Item
     label.items.push(self) unless label.items.include?(self)
   end
 
+  def add_author(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
   private
 
   def can_be_archived?
